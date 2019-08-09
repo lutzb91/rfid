b436306653e8154f27f4b9398a31d53ba9b864db
Lutz <lbahr91@googlemail.com>
2019-08-09 15:42:02 +0200

    SPI as Variable

--- src/MFRC522.h
+++ src/MFRC522.h
@@ -337,7 +337,7 @@ public:
 	MFRC522();
 	DEPRECATED_MSG("use MFRC522(byte chipSelectPin, byte resetPowerDownPin)")
 	MFRC522(byte resetPowerDownPin);
-	MFRC522(byte chipSelectPin, byte resetPowerDownPin);
+	MFRC522(byte chipSelectPin, byte resetPowerDownPin, SPIClass& spi);
 	
 	/////////////////////////////////////////////////////////////////////////////////////
 	// Basic interface functions for communicating with the MFRC522
@@ -437,6 +437,7 @@ protected:
 	byte _chipSelectPin;		// Arduino pin connected to MFRC522's SPI slave select input (Pin 24, NSS, active low)
 	byte _resetPowerDownPin;	// Arduino pin connected to MFRC522's reset and power down input (Pin 6, NRSTPD, active low)
 	StatusCode MIFARE_TwoStepHelper(byte command, byte blockAddr, int32_t data);
+	SPIClass& _spi;
 };
 
 #endif
